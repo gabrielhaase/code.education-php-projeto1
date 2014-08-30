0bdaad1fcef95189628039139f8486799d09fcf1
Gabriel Haase de Azevedo <gabrielhaase@gmail.com>
2014-08-30 14:02:31 -0300

    Inclusão da sessão CONTATO.

---
+++ includes/contato.php
@@ -0,0 +1,10 @@
+<div class="page-header">
+  <h1><span class="tweak-width">Contato</span><small>Site simples em PHP</small></h1>
+</div>
+<div class="image">
+	<img src="./library/images/contact.jpg"/>
+</div>
+<div class="content">
+	<span class="title">Lorem ipsum dolor sit amet</span>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce dictum commodo leo, sit amet scelerisque nisi sodales ac. Suspendisse tristique egestas vehicula. Aliquam nec velit luctus est laoreet convallis eleifend nec elit. Maecenas non leo sed dui interdum laoreet a ultricies risus. Nulla facilisi. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at urna non mi egestas vestibulum faucibus ac ante. Nunc hendrerit justo a justo facilisis, vel posuere erat volutpat. Pellentesque facilisis eget lorem a egestas.</p>
+</div>
