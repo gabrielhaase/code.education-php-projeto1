4558e4e2871f018422abcddfb182e08597bd4580
Gabriel Haase de Azevedo <gabrielhaase@gmail.com>
2014-08-30 12:30:37 -0300

    Inclusão de arquivo head.php contendo tags necessárias à página.

---
+++ includes/head.php
@@ -0,0 +1,8 @@
+<meta http-equiv="X-UA-Compatible" content="IE=edge">        
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />	
+
+<link href="./library/bootstrap-3.2.0/css/bootstrap.min.css" rel="stylesheet">
+<link href="./library/css/style.css" media="screen" rel="stylesheet" type="text/css" />
+
+<script src="//code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+<script src="./library/bootstrap-3.2.0/js/bootstrap.min.js"></script>
--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>code.education - PHP - Projeto 1</title>
+		<?php require_once('./includes/head.php'); ?>
 	</head>
 	<body>
 	</body>
