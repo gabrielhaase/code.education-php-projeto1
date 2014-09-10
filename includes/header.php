63212825b9e00e6cd571e4b551f9f0af482c5b04
Gabriel Haase de Azevedo <gabrielhaase@gmail.com>
2014-09-10 10:07:03 -0300

    Inclusão de navegação dinâmica baseada no conteúdo da base de dados.

--- includes/header.php
+++ includes/header.php
@@ -1,18 +1,24 @@
+<?php
+//----------------------------------------------------------------------------------------------------
+$navItems = 'SELECT title, path FROM content';
+$navItems_stmt = $pdoConnection->prepare($navItems);
+$navItems_stmt->execute();
+$navItems_result = $navItems_stmt->fetchAll(PDO::FETCH_ASSOC);
+//----------------------------------------------------------------------------------------------------
+?>
 <header>
 	<div class="background-bar">
 		<div class="brand"><img src="library/images/marca.png" /></div>
 		<div class="project-description">
 			<div class="brand"><img src="library/images/logo-php-80x42.png" /></div>
-			<span class="description">Projeto 1</span>
+			<span class="description">Projeto Fase 3</span>
 		</div>	
 	</div>
 	<nav>
 		<ul class="nav nav-pills nav-justified">
-			<li class="<?php if(empty($selectedPath)): ?>active<?php endif;?>"><a href="/">Home</a></li>
-			<li class="<?php if($selectedPath=="empresa"): ?>active<?php endif;?>"><a href="empresa">Empresa</a></li>
-			<li class="<?php if($selectedPath=="produtos"): ?>active<?php endif;?>"><a href="produtos">Produtos</a></li>
-			<li class="<?php if($selectedPath=="servicos"): ?>active<?php endif;?>"><a href="servicos">Serviços</a></li>
-			<li class="<?php if($selectedPath=="contato"): ?>active<?php endif;?>"><a href="contato">Contato</a></li>
+			<?php foreach($navItems_result AS $navItem):?>
+				<li class="<?php if($targetPath==$navItem['path']): ?>active<?php endif;?>"><a href="<?php echo $navItem['path']; ?>"><?php echo $navItem['title']; ?></a></li>
+			<?php endforeach;?>
 		</ul>
 	</nav>
 </header>
