b12a2209505ab7eea6e560825418f6f56a939974
Gabriel Haase de Azevedo <gabrielhaase@gmail.com>
2014-09-10 14:03:35 -0300

    Adequação no arquivo de conexão com a base de dados.

--- connection/pdoConnection.php
+++ connection/pdoConnection.php
@@ -1,9 +1,13 @@
 <?php
+//----------------------------------------------------------------------------------------------------
+$hostname = 'localhost';
+$database = 'code.education-PHP';
+$username = 'root';
+$password = '70ga221002';
+
 try {
-	$databaseName = 'code.education-PHP';
-	$username = 'root';
-	$password = '70ga221002';
-    $pdoConnection = new \PDO('mysql:host=localhost;dbname='.$databaseName.';charset=utf8', $username ,$password );
+    $pdoConnection = new \PDO('mysql:host='.$hostname.';dbname='.$database.';charset=utf8', $username ,$password );
 } catch (\PDOException $e) {
     die("Error: ".$e->getCode() . ": " . $e->getMessage());
 }
+//----------------------------------------------------------------------------------------------------
